5c0961cdb30195b74eedaae26660767f2818af9e
Mafianek <ddressler@seznam.cz>
2014-12-07 23:42:17 +0100

    Hotový obchod

--- RPG game GUI/RPG game GUI/Character/shop.xaml.cs
+++ RPG game GUI/RPG game GUI/Character/shop.xaml.cs
@@ -48,6 +48,48 @@ namespace RPG_game_GUI.Character
             Canvas.SetTop(this.Parent as Viewbox, Canvas.GetTop(this.Parent as Viewbox) + e.VerticalChange);
         }
 
+        private void Label6_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var pom = money.Content;
+            int pom2 = Convert.ToInt32(pom);
+            money.Content = pom2 - 7000;
+        }
+
+        private void Label5_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var pom = money.Content;
+            int pom2 = Convert.ToInt32(pom);
+            money.Content = pom2 - 5000;
+        }
+
+        private void Label4_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var pom = money.Content;
+            int pom2 = Convert.ToInt32(pom);
+            money.Content = pom2 - 20000;
+        }
+
+        private void Label3_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var pom = money.Content;
+            int pom2 = Convert.ToInt32(pom);
+            money.Content = pom2 - 1000;
+        }
+
+        private void Label2_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var pom = money.Content;
+            int pom2 = Convert.ToInt32(pom);
+            money.Content = pom2 - 450;
+        }
+
+        private void Label1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var pom = money.Content;
+            int pom2 = Convert.ToInt32(pom);
+            money.Content = pom2 - 500;
+        }
+
     }
 }
 
